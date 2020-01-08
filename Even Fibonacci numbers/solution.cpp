813f59c103951f787890b42a1d55a24e39db995b
Benjamin <namelast49@gmail.com>
2020-01-08 00:51:10 -0500

    the golden ratio

--- Even Fibonacci numbers/solution.cpp
+++ Even Fibonacci numbers/solution.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main(){
-    
+    int n = 3;
+    float golden_ratio = (sqrt(5) +1)/2;
+    int sum = 0;
+    int limit = 4e6;
+    int F_n = 0;
+    cout << limit << endl;
+    while (F_n < limit){
+        sum+=F_n;
+        F_n = floor(pow(golden_ratio, n)/sqrt(5) + 0.5);
+        n+=3;
+        
+    }
+    cout << "nth term is : " << + F_n  << " cumulative sum is " << sum << endl;
 }
