b277dc63ab28cd9dde2f290872446891797fda36
Benjamin Nwachukwu <namelast49@gmail.com>
2020-01-10 14:26:38 -0500

    added user stats

---
+++ prime_factor/solution.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cmath>
+#include <unordered_map>
+using namespace std;
+unordered_map<long, bool> primes({{2, true}, {3, true}, {5, true}, {7, true}, {11, true}, {13, true}, {17, true}, {19, true}, {23, true}});
+
+bool isPrime(long factor){
+    bool Found = primes.find(factor) != primes.end();
+
+    if(!Found){
+        long sqrt_num = floor(sqrt(factor));
+        isPrime(sqrt_num);
+        for(long num =sqrt_num ; num >=2; --num){
+            if(factor % num == 0){
+                return false;
+            }
+        }
+        primes[factor] = true;
+        return true;
+    
+
+    }
+
+    return true;
+
+}
+int main(){
+    long long composite = 60085147514369242;
+    long long sqrt_factor = floor(sqrt(composite));
+    long result =2;
+    for (long num = sqrt_factor; num>=2; --num){
+        // check if num is a factor of the 600,851,475,143
+        if(composite % num == 0){
+            // find the other factor
+            long factor = composite/num;
+            // is either one of them primes
+            if(isPrime(num) || isPrime(factor)){
+                if(isPrime(factor)){
+                    result = factor;
+                    
+                }
+                else{
+                    result = num;
+                }
+                break;
+            }
+        }
+        
+    }
+    cout <<"largest prime factor of 600,851,475,143 " <<result << "\n";
+
+
+}
